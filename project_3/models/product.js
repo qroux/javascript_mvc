c9ec79cbb5115172aac910d0da814dd3fd8f0d6f
quentin roux <quentin.roux@hotmail.fr>
2020-04-21 11:30:09 +0200

    DB and ORM working

--- project_3/models/product.js
+++ project_3/models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const db = require('../util/database')
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -19,22 +20,19 @@ const getProductsFromFile = cb => {
 
 module.exports = class Product {
   constructor(title, imageUrl, description, price) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+    this.title = title
+    this.imageUrl = imageUrl
+    this.description = description
+    this.price = price
+}
 
   save() {
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    )
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    return db.execute('SELECT * FROM products')
   }
 };
